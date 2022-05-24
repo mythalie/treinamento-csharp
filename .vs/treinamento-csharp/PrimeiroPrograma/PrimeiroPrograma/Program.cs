0432458472a700269a5f2333d6d8c4e83fad58a0
Mythalie Sandretti <mythalie.sandretti@iteris.com.br>
2022-05-24 14:13:37 -0300

    Primeiro programa

---
+++ .vs/treinamento-csharp/PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+int numA = int.Parse(a);
+int numB = int.Parse(b);
+Console.WriteLine("A soma é: " + (numA + numB));
+
+-------
+
+Console.WriteLine("Digite o primeiro número: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma é: " + (a + b));
+*/
+
+Console.WriteLine("Digite o primeiro número: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine("A multiplicação é: " + (a * b * c));
